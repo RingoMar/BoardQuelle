3a7a74bb617c758e1cef3ceb6f80d7d10eee5157
Justin Hernandez <hernandez.justin01@hotmail.com>
2019-03-23 17:20:09 -0400

    Cool edits

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 ﻿// def file - https://instaud.io/_/3srP.mp3
+
+window.onload = bttvemotes;
 const sounds = {
   "UHHHHHGHHH":"https://instaud.io/_/3srI.mp3",
   "UHHHHHGHHH (Oy remix)":"https://instaud.io/_/3srK.mp3",
@@ -42,3 +44,10 @@ function stopAudio(){
       stop.currentTime = 0;
     }
 };
+
+var bbtv = new Array("https://cdn.betterttv.net/emote/5b1c23eaae1b166cf54e203e/2x","https://cdn.betterttv.net/emote/5a5ed26e6bcd9a06f4fc7de6/2x","https://cdn.betterttv.net/emote/5b47a5af0f10311f6e9cf8e3/2x", "https://cdn.betterttv.net/emote/5b47a5c90f10311f6e9cf8e4/2x", "https://cdn.betterttv.net/emote/5b915f874cc67c7ad8196855/2x","https://cdn.betterttv.net/emote/5b9ab54fe87c0e590d167aba/2x","https://cdn.betterttv.net/emote/5ba302062192445767f61a0a/2x","https://cdn.betterttv.net/emote/5bcf5c2c4d02f55ed86a7361/2x","https://cdn.betterttv.net/emote/5c1dc8c0f7fd1c1c345d1e7f/2x","https://cdn.betterttv.net/emote/5c63a3161cbe12203117ac22/2x","https://cdn.betterttv.net/emote/5992549c6782b936bdeafe1b/2x");
+
+function bttvemotes() {
+  var randomNum = Math.floor(Math.random() * bbtv.length);
+  document.getElementById("gifmeme").src = bbtv[randomNum];
+    }
