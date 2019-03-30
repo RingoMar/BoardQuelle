c26c706e6f7de5083305651fe7f07ec857bcea51
Justin Hernandez <hernandez.justin01@hotmail.com>
2019-03-30 17:01:36 -0400

    New buttons!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 ﻿// def file - https://instaud.io/_/3sVs.mp3
 
 window.onload = bttvemotes
+// var slen = 0;
 const sounds = {
   'BABYSHARK': 'https://instaud.io/_/3t5c.mp3',
   'UHHHHHGHHH': 'https://instaud.io/_/3srI.mp3',
@@ -53,30 +54,39 @@ let board = document.getElementById('board')
 for (let title of Object.keys(audios)) {
   let button = document.createElement('button')
   button.textContent = title
+  button.setAttribute("id", title)
   button.dataset['audio'] = title
+  button.onclick = function(id){
+    let audio = audios[event.target.dataset['audio']]
+    adur = Math.round(audio.duration)
+    document.getElementById("time").innerHTML = adur;
+    document.getElementById("lastplay").innerHTML = id.target.id;
+  }
   board.appendChild(button)
 }
-
-board.addEventListener('click', function (event) {
-  let audio = audios[event.target.dataset['audio']]
-  if (audio) {
-    for (let audio of Object.values(audios)) {
-      audio.pause()
-      audio.currentTime = 0
+  
+  board.addEventListener('click', function (event) {
+    let audio = audios[event.target.dataset['audio']]
+    if (audio) {
+      for (let audio of Object.values(audios)) {
+        audio.pause()
+        audio.currentTime = 0
+      }
+      audio.play()
     }
-    audio.play()
-  }
 })
 
 function stopAudio () {
   let stop = audios[event.target.dataset['audio']]
+  document.getElementById("time").innerHTML = "--";
+  document.getElementById("lastplay").innerHTML = "-----";
   for (let stop of Object.values(audios)) {
     stop.pause()
     stop.currentTime = 0
   }
 }
 
-var bbtv = new Array('https://cdn.betterttv.net/emote/5b1c23eaae1b166cf54e203e/3x', 'https://cdn.betterttv.net/emote/5a5ed26e6bcd9a06f4fc7de6/3x', 'https://cdn.betterttv.net/emote/5b47a5af0f10311f6e9cf8e3/3x', 'https://cdn.betterttv.net/emote/5b47a5c90f10311f6e9cf8e4/3x', 'https://cdn.betterttv.net/emote/5b915f874cc67c7ad8196855/3x', 'https://cdn.betterttv.net/emote/5b9ab54fe87c0e590d167aba/3x', 'https://cdn.betterttv.net/emote/5ba302062192445767f61a0a/3x', 'https://cdn.betterttv.net/emote/5bcf5c2c4d02f55ed86a7361/3x', 'https://cdn.betterttv.net/emote/5c1dc8c0f7fd1c1c345d1e7f/3x', 'https://cdn.betterttv.net/emote/5c63a3161cbe12203117ac22/3x', 'https://cdn.betterttv.net/emote/5992549c6782b936bdeafe1b/3x','https://cdn.betterttv.net/emote/5a5ed3456bcd9a06f4fc7dee/3x','https://cdn.betterttv.net/emote/5bcf5c024d02f55ed86a735d/3x','https://cdn.discordapp.com/emojis/454150085077499915.gif?v=1','https://cdn.discordapp.com/emojis/453335514154795009.png?v=1','https://cdn.discordapp.com/emojis/490641513835921418.png?v=1')
+var bbtv = new Array('https://cdn.betterttv.net/emote/5b1c23eaae1b166cf54e203e/3x', 'https://cdn.betterttv.net/emote/5a5ed26e6bcd9a06f4fc7de6/3x', 'https://cdn.betterttv.net/emote/5b47a5af0f10311f6e9cf8e3/3x', 'https://cdn.betterttv.net/emote/5b47a5c90f10311f6e9cf8e4/3x', 'https://cdn.betterttv.net/emote/5b915f874cc67c7ad8196855/3x', 'https://cdn.betterttv.net/emote/5b9ab54fe87c0e590d167aba/3x', 'https://cdn.betterttv.net/emote/5ba302062192445767f61a0a/3x', 'https://cdn.betterttv.net/emote/5bcf5c2c4d02f55ed86a7361/3x', 'https://cdn.betterttv.net/emote/5c1dc8c0f7fd1c1c345d1e7f/3x', 'https://cdn.betterttv.net/emote/5c63a3161cbe12203117ac22/3x', 'https://cdn.betterttv.net/emote/5992549c6782b936bdeafe1b/3x','https://cdn.betterttv.net/emote/5a5ed3456bcd9a06f4fc7dee/3x','https://cdn.betterttv.net/emote/5bcf5c024d02f55ed86a735d/3x','https://cdn.discordapp.com/emojis/454150085077499915.gif?v=1','https://cdn.discordapp.com/emojis/453335514154795009.png?v=1')
 
 function bttvemotes () {
   var randomNum = Math.floor(Math.random() * bbtv.length)
