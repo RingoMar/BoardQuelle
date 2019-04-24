9883d4b331675edd4abe52b42e64d10821c3d3a2
Justin Hernandez <hernandez.justin01@hotmail.com>
2019-04-23 23:34:21 -0400

    HAWT NEW FILTERS

---
+++ js/filterengine.js
@@ -0,0 +1,147 @@
+// LEWD
+var b1 = document.getElementById("ZOEY'S-P");
+var b2 = document.getElementById("THE-NOODS");
+var b3 = document.getElementById("PIANO");
+var b4 = document.getElementById("ZAQSIGH");
+var b5 = document.getElementById("KERFUFFLES");
+var b6 = document.getElementById("CLEANING-THE-AMBO");
+var b7 = document.getElementById("BACKSIDE");
+var b8 = document.getElementById("ZAQ-PARKING-AMBO");
+var b9 = document.getElementById("CAN-I-FIT");
+var b10 = document.getElementById("THAT-69");
+var b11 = document.getElementById("ZAQGASM");
+var b12 = document.getElementById("MY-NIBS");
+var b13 = document.getElementById("CRUSH-PUSS");
+var b14 = document.getElementById("2-AT-A-TIME");
+var b15 = document.getElementById("WHEEEEW");
+//  add the class
+b1.setAttribute("class", "lewd");
+b2.setAttribute("class", "lewd");
+b3.setAttribute("class", "lewd");
+b4.setAttribute("class", "lewd");
+b5.setAttribute("class", "lewd");
+b6.setAttribute("class", "lewd");
+b7.setAttribute("class", "lewd");
+b8.setAttribute("class", "lewd");
+b9.setAttribute("class", "lewd");
+b10.setAttribute("class", "lewd");
+b11.setAttribute("class", "lewd");
+b12.setAttribute("class", "lewd");
+b13.setAttribute("class", "lewd");
+b14.setAttribute("class", "lewd");
+b15.setAttribute("class", "lewd");
+
+function lewd() {
+  $("#board button").css("display", "inline-flex");
+  $("#board button").not(".lewd").css("display", "none");
+}
+// LEWD
+
+// fun
+var fun1 = document.getElementById("SNORT");
+var fun2 = document.getElementById("THE-DERP-KNIGHT");
+var fun3 = document.getElementById("SNEEZE");
+var fun4 = document.getElementById("BELELELELE-NO!");
+var fun5 = document.getElementById("THE-NOODS");
+var fun6 = document.getElementById("ZAQWIZ");
+var fun7 = document.getElementById("HAYES-HELICOPTER");
+var fun9 = document.getElementById("HEY-JERRY");
+var fun10 = document.getElementById("WHAT-STRUTTIN-LIKES");
+var fun11 = document.getElementById("SELFIE");
+var fun12 = document.getElementById("ZAQWOOKIE");
+var fun13 = document.getElementById("ZAQFITE");
+var fun14 = document.getElementById("ZAQUACKLE");
+var fun15 = document.getElementById("ZAQFISH");
+var fun16 = document.getElementById("TURNIN-THE-TURNIPS");
+var fun17 = document.getElementById("Hazer");
+var fun18 = document.getElementById("HAYRPES");
+var fun19 = document.getElementById("HASING");
+var fun20 = document.getElementById("RA..ZAQUELLE");
+var fun21 = document.getElementById("SUH-DUDE");
+var fun22 = document.getElementById("ZAQUELLE'S-LAND");
+var fun23 = document.getElementById("HUUUUU");
+var fun24 = document.getElementById("ZAQHEHEHEHE");
+var fun25 = document.getElementById("ZAQDOLPHIN");
+
+fun1.setAttribute("class", "fun");
+fun2.setAttribute("class", "fun");
+fun3.setAttribute("class", "fun");
+fun4.setAttribute("class", "fun");
+fun5.setAttribute("class", "fun");
+fun6.setAttribute("class", "fun");
+fun7.setAttribute("class", "fun");
+fun9.setAttribute("class", "fun");
+fun10.setAttribute("class", "fun");
+fun11.setAttribute("class", "fun");
+fun12.setAttribute("class", "fun");
+fun13.setAttribute("class", "fun");
+fun14.setAttribute("class", "fun");
+fun15.setAttribute("class", "fun");
+fun16.setAttribute("class", "fun");
+fun17.setAttribute("class", "fun");
+fun18.setAttribute("class", "fun");
+fun19.setAttribute("class", "fun");
+fun20.setAttribute("class", "fun");
+fun21.setAttribute("class", "fun");
+fun22.setAttribute("class", "fun");
+fun23.setAttribute("class", "fun");
+fun24.setAttribute("class", "fun");
+fun25.setAttribute("class", "fun");
+
+
+
+function fun() {
+  $("#board button").css("display", "inline-flex");
+  $("#board button").not(".fun").css("display", "none");
+}
+// fun
+
+
+// Songs
+var bs = document.getElementById("BABYSHARK");
+var bv = document.getElementById("UHHHHHGHHH-(Oy-remix)");
+var cs = document.getElementById("CANDY-SHOP");
+bv.setAttribute("class", "song");
+cs.setAttribute("class", "song");
+bs.setAttribute("class", "song");
+function song() {
+  $("#board button").css("display", "inline-flex");
+  $("#board button").not(".song").css("display", "none");
+}
+// Songs
+
+// spam -able
+var sp1 = document.getElementById("SNEEZE");
+var sp2 = document.getElementById("ZAQYES");
+var sp3 = document.getElementById("CLEANING-THE-AMBO");
+var sp4 = document.getElementById("ZAQUACKLE");
+var sp5 = document.getElementById("HAYRPES");
+var sp6 = document.getElementById("ZAQSHORT");
+var sp7 = document.getElementById("THAT-69");
+var sp8 = document.getElementById("ZAQGASM");
+var sp9 = document.getElementById("ZAQHEHEHEHE");
+var sp10 = document.getElementById("THE-DERP-KNIGHT");
+
+sp1.setAttribute("class", "spamc");
+sp2.setAttribute("class", "spamc");
+sp3.setAttribute("class", "spamc");
+sp4.setAttribute("class", "spamc");
+sp5.setAttribute("class", "spamc");
+sp6.setAttribute("class", "spamc");
+sp7.setAttribute("class", "spamc");
+sp8.setAttribute("class", "spamc");
+sp9.setAttribute("class", "spamc");
+sp10.setAttribute("class", "spamc");
+
+function spam() {
+  $("#board button").css("display", "inline-flex");
+  $("#board button").not(".spamc").css("display", "none");
+}
+// spam -able
+
+/* ########## REST ALL FILTERS ############# */
+
+function reset_f() {
+  $("#board button").css("display", "inline-flex");
+}
+/* ########## REST ALL FILTERS ############# */
